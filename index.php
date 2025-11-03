d2a5d17bb508834b54ad74617317054399253047
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2025-11-03 14:12:18 +0200

    lab4

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
 
+require_once 'Routing.php';
+
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
-var_dump($path);
-
-switch($path){
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'Login':
-        include 'public/views/login.html';
-        break;
-    default:
-    include 'public/views/404.html';
-        break;
-}
-
+Routing::run($path);
