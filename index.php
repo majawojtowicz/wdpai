b2e3c83907a09ba8a163403fcba933fdfd77abe7
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2025-11-03 13:54:04 +0200

    lab2

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "<h1>Hi world</h1>";
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+var_dump($path);
+
+switch($path){
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    case 'Login':
+        include 'public/views/login.html';
+        break;
+    default:
+    include 'public/views/404.html';
+        break;
+}
+
