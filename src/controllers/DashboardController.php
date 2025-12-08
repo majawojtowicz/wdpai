2e90866403701f85cb2fef908458ad45e51ba352
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2025-12-08 14:58:52 +0200

    lab9

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -76,8 +76,24 @@ return $this->render("dashboard", [
 ]);
 }
 public function search(){
+    $contentType = isset($_SERVER['CONTENT_TYPE']) ? trim($_SERVER['CONTENT_TYPE']) : '';
+    if ($contentType !== 'application/json') {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid Content-Type']);
+        return;
+    }
+    if(!$this->isPost()){
+        http_response_code(405);
+        echo json_encode(['error' => 'Method Not Allowed']);
+        return;
+    }
+
     header('Content-Type: application/json');
     http_response_code(200);
+
+    $content= trim(file_get_contents("php://input"));
+    $decoded = json_decode($content, true);
+
     $cards=$this->cardsRepository->getCardsByTitle( 'heart');
     echo json_encode($cards);
 }
